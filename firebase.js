1685bb6b25905dca2cb5729810fbef55bc1c7a7e
운로 <lwjmimt@gmail.com>
2026-05-26 20:13:39 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,11 +9,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCwvEU45zXUnJTcISF2CHBb1H5kPvA1UnY",
+  authDomain: "talktalk-c3cbe.firebaseapp.com",
+  databaseURL: "https://talktalk-c3cbe-default-rtdb.firebaseio.com",
+  projectId: "talktalk-c3cbe",
+  storageBucket: "talktalk-c3cbe.firebasestorage.app",
+  messagingSenderId: "476607267300",
+  appId: "1:476607267300:web:1d0ea398b686227c3e87f1",
+  measurementId: "G-HYD9W5XNX0"
 };
 
 const app = initializeApp(firebaseConfig);
