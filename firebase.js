4886e58ce293e8f61b19da0f6cfcdf7af17f6aa6
운로 <lwjmimt@gmail.com>
2026-05-26 19:56:51 +0900

    Refactor Firebase imports and initialization

--- firebase.js
+++ firebase.js
@@ -1,52 +1,18 @@
-import {
-initializeApp
-}
-from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
-import {
-getAuth
-}
-from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
-
-import {
-getFirestore
-}
-from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
-
-import {
-getStorage
-}
-from "https://www.gstatic.com/firebasejs/12.13.0/firebase-storage.js";
-
+// 🔥 본인의 Firebase 프로젝트 설정 값을 입력하세요!
 const firebaseConfig = {
-
-apiKey:
-"YOUR_API_KEY",
-
-authDomain:
-"YOUR_AUTH_DOMAIN",
-
-projectId:
-"YOUR_PROJECT_ID",
-
-storageBucket:
-"YOUR_STORAGE_BUCKET",
-
-messagingSenderId:
-"YOUR_MESSAGING_SENDER_ID",
-
-appId:
-"YOUR_APP_ID"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-const app =
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export const auth =
-getAuth(app);
-
-export const db =
-getFirestore(app);
-
-export const storage =
-getStorage(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
